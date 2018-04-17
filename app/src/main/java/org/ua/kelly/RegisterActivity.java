023bf87e5d9abd2e23070638d0de5e0dbc7c5263
trashg0d <wang.e934@gmail.com>
2018-04-17 17:56:41 -0400

    helo

--- app/src/main/java/org/ua/kelly/RegisterActivity.java
+++ app/src/main/java/org/ua/kelly/RegisterActivity.java
@@ -14,7 +14,6 @@ public class RegisterActivity extends AppCompatActivity {
 
         final EditText etAge = (EditText) findViewById(R.id.etAge);
         final EditText etName = (EditText) findViewById(R.id.etName);
-        final EditText etEmail = (EditText) findViewById(R.id.etEmail);
         final EditText etUsername = (EditText) findViewById(R.id.etUsername);
         final EditText etPassword = (EditText) findViewById(R.id.etPassword);
         final Button bRegister = (Button) findViewById(R.id.bRegister);
